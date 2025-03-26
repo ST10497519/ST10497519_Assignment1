c773ce2ebc66fcb995b0732e2c0caf21426e4418
ST10497519 <ST10497519@vconnect.edu.za>
2025-03-26 11:25:25 +0200

    Added in AlertDialog to give examples and tell user what to enter.

--- app/src/main/java/za/co/varsitycollege/st10497519/st10497519_assignment1/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10497519/st10497519_assignment1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -25,5 +26,28 @@ class MainActivity : AppCompatActivity() {
         suggestionText = findViewById(R.id.suggestionTextView)
         resetButton = findViewById(R.id.resetbutton)
 
+        suggestButton.setOnClickListener {
+
+            // Time of day from input
+            val timeOfDay = timeInput.text.toString().trim().lowercase()
+            // Valid times of the day able to be entered
+            val validTimesOfDay =
+                listOf("morning", "mid-morning", "afternoon", "mid-afternoon", "dinner")
+
+            if (timeOfDay.isEmpty() || !validTimesOfDay.contains(timeOfDay)) {
+                // Show AlertDialog if input is empty
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Input Required")
+                builder.setMessage("Please enter a valid time of day (e.g., morning, mid-morning, afternoon, mid-afternoon, dinner)")
+                builder.setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss() // Dismiss dialog when OK is clicked
+                }
+                builder.create().show()
+
+                suggestionText.text = "Please enter a time of day!"
+                return@setOnClickListener
+            }
+
         }
     }
+}
